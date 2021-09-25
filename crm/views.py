9ac1d066dafe930aa145b99d73d75e6a8d44a559
zile <cfzile@gmail.com>
2021-09-25 23:21:34 +0300

    debug

--- crm/views.py
+++ crm/views.py
@@ -39,6 +39,8 @@ def get_full_context(request, context):
         scheduled_by_user = Schedule.objects.all().filter(owner=auth_profile.user.id)
         tasks = Task.objects.all().filter(Q(owner_id=request.user.id) | Q(executor_id=request.user.id))
         print(auth_profile.prototype)
+        for com in competences:
+            print(com)
         if auth_profile.prototype != -1:
             competences = competences.filter(Q(prototype=auth_profile.prototype))
             grade_templates = grade_templates.filter(Q(prototype=auth_profile.prototype))
